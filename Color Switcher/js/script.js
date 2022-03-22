b47baa4df163de8d4f0fd7352732261e4de261a6
limchaekyung <corud505@naver.com>
2022-03-22 23:52:28 +0900

    payment form

--- Color Switcher/js/script.js
+++ Color Switcher/js/script.js
@@ -3,9 +3,9 @@ let themeToggler = document.querySelector('.theme-toggler')
 themeToggler.onclick = () => {
     themeToggler.classList.toggle('active')
 
-    if(themeToggler.classList.contains('active')){
+    if (themeToggler.classList.contains('active')) {
         document.body.classList.add('active')
-    }else{
+    } else {
         document.body.classList.remove('active')
     }
 }
