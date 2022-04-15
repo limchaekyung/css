d5ae9cee6232b0f947658395a56b8b1e174cec9b
limchaekyung <corud505@naver.com>
2022-04-15 23:57:52 +0900

    popup modal2

---
+++ Popup modal/asset/js/main.js
@@ -0,0 +1,13 @@
+const showModal = (openButton, modalContent) => {
+    const openBtn = document.getElementById(openButton),
+        modalContainer = document.getElementById(modalContent)
+
+    if (openBtn && modalContainer) {
+        openBtn.addEventListener('click', () => {
+            modalContainer.classList.add('show-modal')
+        })
+    }
+}
+
+showModal('open-modal', 'modal-container')
+
