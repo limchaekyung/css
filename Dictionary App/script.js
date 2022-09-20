8f8ea1c8f4c071709600f102743f8a04ac6868ee
limchaekyung <corud505@naver.com>
2022-09-20 23:56:05 +0900

    dictionary api

---
+++ Dictionary App/script.js
@@ -0,0 +1,25 @@
+const wrapper = document.querySelector(".wrapper");
+searchInput = wrapper.querySelector("input");
+infoText = wrapper.querySelector(".info-text");
+
+function data(result, word) {
+  if (result.title) {
+    infoText.innerHTML = `Can't find the meaning of <span>"${word}"</span>. Please, try to search for another word.`;
+  } else {
+    console.log(result);
+  }
+}
+function fetchApi(word) {
+  infoText.style.color = "#000";
+  infoText.innerHTML = `Searching the meaning of <span>"${word}"</span>`;
+  let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((result) => data(result, word));
+}
+
+searchInput.addEventListener("keyup", (e) => {
+  if (e.key === "Enter" && e.target.value) {
+    fetchApi(e.target.value);
+  }
+});
