33ec2162b1dd1e1639dba9043ec53b3602e8902c
limchaekyung <corud505@naver.com>
2022-10-01 23:58:49 +0900

    image editor filter

--- Image Editor/script.js
+++ Image Editor/script.js
@@ -1,13 +1,43 @@
 const fileInput = document.querySelector(".file-input");
+filterOptions = document.querySelectorAll(".filter button");
+filterName = document.querySelector(".filter-info .name");
+filterValue = document.querySelector(".filter-info .value");
+filterSlider = document.querySelector(".slider input");
 previewImg = document.querySelector(".preview-img img");
 chooseImgBtn = document.querySelector(".choose-img");
+
+let brightness = 100,
+  saturation = 100,
+  inversion = 0,
+  grayscale = 0;
 
 const loadImage = () => {
   let file = fileInput.files[0];
   if (!file) return;
   previewImg.src = URL.createObjectURL(file);
+  previewImg.addEventListener("load", () => {
+    document.querySelector(".container").classList.remove("disable");
+  });
 };
 
+filterOptions.forEach((option) => {
+  option.addEventListener("click", () => {
+    document.querySelector(".filter .active").classList.remove("active");
+    option.classList.add("active");
+    filterName.innerText = option.innerText;
+  });
+});
+
+const updateFilter = () => {
+  filterValue.innerText = `${filterSlider.value}%`;
+  const selectedFilter = document.querySelector(".filter .active");
+
+  if (selectedFilter.id === "brightness") {
+    brightness = filterSlider.value;
+  }else if(selectedFilter.id === "saturation")
+};
+
+fileSlider.addEventListener("input", updateFilter);
 fileInput.addEventListener("change", loadImage);
 chooseImgBtn.addEventListener("click", () => {
   fileInput.click();
